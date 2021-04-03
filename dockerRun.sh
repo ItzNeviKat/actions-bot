c35f56bd07a2984efbec36a98917504d50d12d21
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-04-03 19:23:14 +0300

    chore(script): Порт

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,4 +1,4 @@
 docker stop actions-bot
 docker rm actions-bot
 docker build -t actions-bot .
-docker run -d -p3020:3020 --name actions-bot -e PORT=3030 actions-bot
+docker run -d -p3030:3030 --name actions-bot -e PORT=3030 actions-bot
