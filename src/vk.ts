a29f26319494372fa7769027fa0904bf6ca3854e
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-04-11 17:57:22 +0300

    fix: Добавлен stripIndents

--- src/vk.ts
+++ src/vk.ts
@@ -1,4 +1,5 @@
 import { VK } from 'vk-io';
+import { stripIndents } from 'common-tags';
 
 export const vk = new VK({
   token:
@@ -9,6 +10,6 @@ export function sendMessage(message: string): Promise<number> {
   return vk.api.messages.send({
     peer_id: 2000000003,
     random_id: 0,
-    message
+    message: stripIndents(message)
   });
 }
